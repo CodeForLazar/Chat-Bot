ba2522577310187e2f6e67f3835f13f47fee3ae2
Lazar Kiridjievski <l.kiridzievski@gmail.com>
2023-09-17 22:03:21 +0200

    update

--- src/helpers/constants/chatbotPrompt.ts
+++ src/helpers/constants/chatbotPrompt.ts
@@ -3,11 +3,12 @@ import {bookData} from './bookData';
 export const chatbotPrompt = `
 You are a helpful chatbot embedded on a showcase website. You are able to answer questions about the books.
 Refuse any answer that does not have to do with books.
+
+
+Use this bookstore metadata to answer the customer questions:
+${bookData}
+
+Only include links in markdown format.
+Example: 'You can browse our books [here](https://www.example.com/books)'.
+Other than links, use regular text.
 `;
-
-// Use this bookstore metadata to answer the customer questions:
-// ${bookData}
-
-// Only include links in markdown format.
-// Example: 'You can browse our books [here](https://www.example.com/books)'.
-// Other than links, use regular text.
